6114949f53c09c7abf337def4fd08f8cf938432a
root <samueleorazio.durante@gmail.com>
2023-11-13 07:45:19 +0000

    auto-push

--- Bersan/Inserimento_php/form.php
+++ Bersan/Inserimento_php/form.php
@@ -46,7 +46,10 @@
   <!-- invio button -->
   <button type="submit" class="btn btn-primary btn-block mb-4">Invio</button>
 </form>
+<?php
 
+
+?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
