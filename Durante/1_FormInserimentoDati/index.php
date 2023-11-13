801192c878adb97ee566b8e39299592c9cf6da93
root <samueleorazio.durante@gmail.com>
2023-11-13 07:30:15 +0000

    auto-push

--- Durante/1_FormInserimentoDati/index.php
+++ Durante/1_FormInserimentoDati/index.php
@@ -7,30 +7,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body style = "background-color:white">
-    <center><h1>Inserimento studenti</h1><br>       
+    <center><br><br><h1>Inserimento studenti</h1><br>       
     
 <div class="container">
   <div class="row">
-    <div class="col-6 my-2">
+    <div class="col-2">
+      <BR>
+</div>
+    <div class="col-4 my-2">
 <form class="form-horizontal" action="#" method="POST">
   <div class="form-group">
-    <label class="control-label col-sm-2" for="nome">Nome:</label>
-    <div class="col-sm-10 my-2">
+    <label class="control-label" for="nome">Nome</label>
+    <div class="my-2">
       <input type="text" class="form-control" id="nome" name="nome" placeholder="Inserisci nome">
     </div>
   </div>
 </div>
-<div class="col-6 my-2">
+<div class="col-4 my-2">
   <div class="form-group">
-    <label class="control-label" for="cognome">Cognome:</label>
-    <div class="col-sm-10 my-2">
+    <label class="control-label" for="cognome">Cognome</label>
+    <div class="my-2">
       <input type="text" class="form-control" id="cognome" name="cognome" placeholder="Inserisci cognome">
     </div>
   </div>
 </div>
 </div>
 <div class="row">
-  <div class="col-3">
+<div class="col-2">
+      <BR>
+</div>
+  <div class="col-2">
   <div class="form-group my-2">
       <label for="classe">Classe</label>
       <select id="classe" name="classe" class="form-control">
@@ -43,16 +49,16 @@
       </select>
     </div>
 </div>
-<div class="col-3">
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="sezione">Sezione:</label>
-    <div class="col-sm-3 my-2">
+<div class="col-2">
+  <div class="form-group my-2">
+    <label class="control-label" for="sezione">Sezione</label>
+    <div class="form">
       <input type="text" class="form-control" id="sezione" name="sezione" placeholder="Inserisci sezione">
     </div>
   </div>
 </div>
-  <div class="col-3">
-  <div class="form-group">
+  <div class="col-4">
+  <div class="form-group my-2">
       <label for="indirizzo">Indirizzo</label>
       <select id="indirizzo" name="indirizzo" class="form-control">
         <option selected value="0">Scegli l'indirizzo</option>
@@ -77,7 +83,7 @@
 </div>
 </div>
 </div>
-</center>
+
   <?php
       include "connessione.php";  
       if(isset($_POST["submit_btn"])){
@@ -119,7 +125,7 @@
         }                                                                     
       }
     ?>
-
+</center>
 
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
