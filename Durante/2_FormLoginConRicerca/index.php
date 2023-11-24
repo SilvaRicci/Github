7a279aecee4c87fcdefc686b321d914739b15c9e
root <samueleorazio.durante@gmail.com>
2023-11-24 08:57:00 +0000

    auto-push

--- Durante/2_FormLoginConRicerca/index.php
+++ Durante/2_FormLoginConRicerca/index.php
@@ -7,12 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body style = "background-color:white">
-    <center><br><br><h1>Hub azienda</h1><br>       
-        <a href="registrazione.php"> Registrazione cliente </a>
-        <a href="visualizzazione.php"> Visualizzazione clienti registrati </a>
-        <a href="login.php"> Login</a>
-        <a href="ricercaSimple.php"> Ricerca semplice</a>
-        <a href="ricercaAdvanced.php"> Ricerca avanzata</a>
+    <center><br><br><h1>Hub azienda</h1><br>      
+        <a class="btn btn-primary" role="button" href="registrazione.php"> Registrazione cliente </a> <br><br>
+        <a class="btn btn-primary" role="button"  href="visualizzazione.php"> Visualizzazione clienti registrati </a> <br><br>
+        <a class="btn btn-primary" role="button"  href="login.php"> Login</a> <br><br>
+        <a class="btn btn-primary" role="button"  href="ricercaSimple.php"> Ricerca semplice</a> <br><br>
+        <a class="btn btn-primary" role="button"  href="ricercaAdvanced.php"> Ricerca avanzata</a> <br><br>
     </center>
 
   
