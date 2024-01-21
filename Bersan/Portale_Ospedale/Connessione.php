6a20f2b08a9cc2de16cca9040cb4c65943b5690e
root <samueleorazio.durante@gmail.com>
2024-01-21 18:58:33 +0000

    auto-push

---
+++ Bersan/Portale_Ospedale/Connessione.php
@@ -0,0 +1,10 @@
+<?php
+$db="Portale_Ospedale"; //nome db
+$db_host="localhost";
+$db_user = "root";
+$db_password="";
+$db_connection = new mysqli($db_host, $db_user, $db_password, $db);
+if($db_connection->connect_error){
+    die("Si e' verificato il seguente problema tecnico" . $db_connection->connect_error);
+}
+?>
