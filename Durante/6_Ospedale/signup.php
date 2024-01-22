d7adaf345bc639b954bca2784675053a381714b4
root <samueleorazio.durante@gmail.com>
2024-01-22 07:16:35 +0000

    auto-push

--- Durante/6_Ospedale/signup.php
+++ Durante/6_Ospedale/signup.php
@@ -3,12 +3,97 @@
 <!doctype html>
     <html lang="en">
     <?php
-        include ""
+        include "connessione.php";
         session_start();
         if(isset($_SESSION['id'])){
             header("Location: home.php");
         }
     ?>
+
+    <?php
+        function signup(){
+
+            include "connessione.php";
+
+            $CF = $_POST["CF"];
+            $cognome = $_POST["cognome"];
+            $nome = $_POST["nome"]; 
+            $indirizzo = $_POST["indirizzo"];
+            $comune = $_POST["comune"];
+            $CAP = $_POST["cap"];
+            $provincia = $_POST["provincia"];
+            $dataNascita = $_POST["dataNascita"];
+            $genere = $_POST["genere"];
+            
+            $password = $db_connection->real_escape_string(stripslashes($_POST["password"]));
+
+            if($isOk){
+                $password = password_hash($password,PASSWORD_DEFAULT);
+
+                $ok=$db_connection->query("INSERT INTO utente (CF,cognome,nome,indirizzo,comune,CAP,provincia,dataNascita,genere,password) VALUES ('$CF','$cognome','$nome','$indirizzo','$comune','$CAP','$provincia','$dataNascita','$genere','$password')");
+                
+                echo "Inserimento dei dati nella tabella: 100% completato.";
+                
+                header("Location: login.php");
+            }
+            
+            $db_connection->close();    
+        }
+
+        function dataVerify($CF,$cognome,$nome,$indirizzo,$comune,$CAP,$provincia,$dataNascita,$genere){
+            $isOk=true;
+            
+            if($CF==""){
+                $isOk=false;
+                echo "Codice fiscale non inserito <br />";
+            }
+            if(!(strlen($CF)==16) && $isOk){
+                $isOk=false;
+                echo "Codice fiscale non valido <br />";
+            }
+            if($cognome==""){
+                $isOk=false;
+                echo "Cognome non inserito <br />";
+            }
+            if($nome==""){
+                $isOk=false;
+                echo "Nome non inserito <br />";
+            }
+            if($indirizzo==""){
+                $isOk=false;
+                echo "Indirizzo non inserito <br />";
+            }
+            if($comune==""){
+                $isOk=false;
+                echo "Comune non inserito <br />";
+            }
+            if($CAP==""){
+                $isOk=false;
+                echo "CAP non inserito <br />";
+            }
+            if($provincia==""){
+                $isOk=false;
+                echo "Provincia non inserita <br />";
+            }
+            if($dataNascita==""){
+                $isOk=false;
+                echo "Data di nascita non inserita <br />";
+            }
+            if($genere=="-1"){
+                $isOk=false;
+                echo "Genere non inserito <br />";
+            }
+            if($password==""){
+            $isOk=false;
+            echo "Password non inserita <br />";
+            }
+            //eventuali nuovi controlli sulla password
+            return $isOk;
+        }
+
+    ?>
+
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
