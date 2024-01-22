aa8725a8ef88993f97124b869e3601db86275e83
root <samueleorazio.durante@gmail.com>
2024-01-22 08:36:57 +0000

    auto-push

--- Durante/6_Ospedale/user/home.php
+++ Durante/6_Ospedale/user/home.php
@@ -28,10 +28,10 @@
   <body style = "background-color:white">
 
   <!-- Inizio navbar -->
-  <nav class="navbar navbar-dark bg-primary">
+  <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
 
     <!-- Torna a home.php -->
-    <a class="navbar-brand" href="home.php">Home</a>
+    <a class="navbar-brand" href="home.php">ISPedale</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -40,35 +40,23 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link active" href="home.php">Home <span class="sr-only"></span></a>
         </li>
         <!-- 2# button -> Vai a profilo.php -->
         <li class="nav-item">
           <a class="nav-link" href="profilo.php">Profilo</a>
         </li>
-        <!-- 1# button -> Vai a profilo.php -->
+        <!-- 3# button -> Vai a visite.php -->
         <li class="nav-item">
-          <a class="nav-link" href="profilo.php">Profilo</a>
+          <a class="nav-link" href="visite.php">Visite</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
+
       </ul>
+      <!-- 
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      </form>-->
     </div>
   </nav>
     
