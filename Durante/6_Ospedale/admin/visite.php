b87bf69b0e0c77a73193f9e1bf167424771c6004
root <samueleorazio.durante@gmail.com>
2024-01-25 21:38:50 +0000

    auto-push

--- Durante/6_Ospedale/admin/visite.php
+++ Durante/6_Ospedale/admin/visite.php
@@ -7,158 +7,6 @@
     if(!isset($_SESSION['username'])){
       header("Location: $LOGIN_PATH");
     }
-  
-
-	function searchUser(){
-
-    include "../config/path.php";
-    include $CONN_PATH;
-    
-    $data = $_POST['inputData'];
-    $type = $_POST['type'];
-
-    $data = "DRMD5345JFJH3446";
-    $type = 0;
-    
-    switch($type){
-      case 0:{
-        $query = "SELECT * FROM utente WHERE CF = '$data'";
-        break;
-      }
-      case 1:{
-        $query = "SELECT * FROM utente WHERE cognome = '$data'";
-        break;
-      }
-      case 2:{
-        $query = "SELECT * FROM utente WHERE nome = '$data'";
-        break;
-      }
-    }
-
-    $resultSearch = $db_connection->query($query);
-
-    printTable($resultSearch);
-
-    $resultSearch->close();
-    $db_connection->close();
-
-  }
-  function searchVisita(){
-    	
-    include "../config/path.php";
-    include $CONN_PATH;
-    
-    $data = $_POST['inputData'];
-    $type = $_POST['type'];
-    
-    switch($type){
-      case 0:{
-        $query = "SELECT * FROM utente WHERE id = '$data'";
-        break;
-      }
-      case 1:{
-        $query = "SELECT * FROM utente WHERE CF_utente = '$data'";
-        break;
-      }
-      case 2:{
-        $query = "SELECT * FROM utente WHERE tipologia = '$data'";
-        break;
-      }
-    }
-    
-    $resultSearch = $db_connection->query($query);
-    
-    printTable($resultSearch);
-
-    $resultSearch->close();
-    $db_connection->close();
-
-  }
-
-  function printTable($data){
-
-    $rows = $data->num_rows;
-
-    if($rows<=0){
-      echo "Temporaneo, nessun risultato trovato";
-      return;
-    }
-
-    echo '<table class="table">
-        <thead>
-            <tr>
-            <th scope="col">Codice fiscale</th>
-            <th scope="col">Cognome</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Indirizzo</th>
-            <th scope="col">Comune</th>
-            <th scope="col">CAP</th>
-            <th scope="col">Data di nascita</th>
-            <th scope="col">Genere</th>
-            <th scope="col">Password</th>
-            <th scope="col">T</th> 
-            </tr>
-        </thead>
-        <tbody>';
-        $row = $data->fetch_assoc();
-    //while($row = $data->fetch_assoc()){
-
-      echo "<tr> <th scope=."."row"."class="."secondary".">". "$row[CF]</th>";
-        echo "<th scope=."."row".">". "$row[cognome] </th>";
-        echo "<th scope=."."row".">". "$row[nome] </th>";
-        echo "<th scope=."."row".">". "$row[indirizzo] </th>";
-        echo "<th scope=."."row".">". "$row[comune] </th>";
-        echo "<th scope=."."row".">". "$row[CAP] </th>";
-        echo "<th scope=."."row".">". "$row[provincia] </th>";
-        echo "<th scope=."."row".">". "$row[dataNascita] </th>";
-        echo "<th scope=."."row".">". "$row[genere] </th>";
-        echo "<th scope=."."row".">". "$row[password] </th>";
-        echo "<th> <form action='deleteUser.php'> <button type='submit' id='deleteUser_btn' name='deleteUser_btn' value='$row[CF]' class='btn btn-danger'><i class='bi bi-trash-fill'></i></button> </form> </th> </tr>";
-    //}
-
-  }
-
-  
-
-
-/*
-  function adminPanel(){
-      include "connessione.php"; 
-      $resultAdmin = $db_connection->query("SELECT * FROM utente");                      
-      $rowsAdmin = $resultAdmin->num_rows;
-
-      echo '<table class="table">
-        <thead>
-            <tr>
-            <th scope="col">Codice fiscale</th>
-            <th scope="col">Cognome</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Indirizzo</th>
-            <th scope="col">Comune</th>
-            <th scope="col">CAP</th>
-            <th scope="col">Data di nascita</th>
-            <th scope="col">Genere</th>
-            <th scope="col">Password</th>
-            <th scope="col">T</th> 
-            </tr>
-        </thead>
-        <tbody>';
-
-      while($row = $resultAdmin->fetch_assoc()){
-        echo "<tr> <th scope=."."row"."class="."secondary".">". "$row[CF]</th>";
-        echo "<th scope=."."row".">". "$row[cognome] </th>";
-        echo "<th scope=."."row".">". "$row[nome] </th>";
-        echo "<th scope=."."row".">". "$row[indirizzo] </th>";
-        echo "<th scope=."."row".">". "$row[comune] </th>";
-        echo "<th scope=."."row".">". "$row[CAP] </th>";
-        echo "<th scope=."."row".">". "$row[provincia] </th>";
-        echo "<th scope=."."row".">". "$row[dataNascita] </th>";
-        echo "<th scope=."."row".">". "$row[genere] </th>";
-        echo "<th scope=."."row".">". "$row[password] </th>";
-        echo "<th> <form action='deleteUser.php'> <button type='submit' id='deleteUser_btn' name='deleteUser_btn' value='$row[CF]' class='btn btn-danger'><i class='bi bi-trash-fill'></i></button> </form> </th> </tr>";
-      }
-  }
-*/
 ?>
 
 
@@ -180,8 +28,8 @@
         <ul class="navbar-nav mr-auto">
         
         <!-- 1# button -> Torna a home.php -->
-        <li class="nav-item active nav-underline px-5 pt-3">
-            <a class="nav-link active nav-underline" href="<?php echo"$ADMIN_PATH"?>">Home <span class="sr-only"></span></a>
+        <li class="nav-item px-5 pt-3">
+            <a class="nav-link" href="<?php echo"$ADMIN_PATH"?>">Home <span class="sr-only"></span></a>
           </li>
 
         <!-- 2# button -> Vai a index.php -->
@@ -205,8 +53,8 @@
           </button>
 
           <!-- 3# button -> Vai a visite.php -->
-          <li class="nav-item px-5 pt-3">
-            <a class="nav-link" href="<?php echo"$AVISITE_PATH"?>">Visite</a>
+          <li class="nav-item active nav-underline px-5 pt-3">
+            <a class="nav-link active nav-underline" href="<?php echo"$AVISITE_PATH"?>">Visite</a>
           </li>
           
           <!-- 4# button -> Vai a profilo.php/logout.php -->
