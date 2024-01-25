a420a37125eb6e24d52c90e7a48d17220ff1e7ba
root <samueleorazio.durante@gmail.com>
2024-01-25 16:13:03 +0000

    auto-push

--- Durante/6_Ospedale/user/home.php
+++ Durante/6_Ospedale/user/home.php
@@ -32,49 +32,63 @@
     
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
 
-      <!-- 4# button -> Vai a index.php -->
-      <ul class="navbar-nav mr-auto px-5 py-1">
-        <li class="nav-item">
-          <a class="nav-link" href="home.php">Panoramica <span class="sr-only"></span></a>
-        </li>
+      <div class="row">
+        <ul class="navbar-nav mr-auto">
 
-      <!-- 1# button -> Torna a home.php -->
-      <ul class="navbar-nav mr-auto px-5 py-1">
-        <li class="nav-item active nav-underline">
-          <a class="nav-link active nav-underline" href="home.php">Home <span class="sr-only"></span></a>
-        </li>
+          <div class="col-1">
+            <!-- 4# button -> Vai a index.php -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="home.php">Panoramica <span class="sr-only"></span></a>
+              </li>
+          </div>
 
-        <!-- Torna a home.php -->
-        <a class="navbar-brand" href="home.php">
-          <img src="<?php echo"$LOGO_PATH"?>" alt="Logo" width="50" height="50">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+          <div class="col-2">
+            <!-- 1# button -> Torna a home.php -->
+            <li class="nav-item active nav-underline">
+              <a class="nav-link active nav-underline" href="home.php">Home <span class="sr-only"></span></a>
+            </li>
+          </div>
 
-        <!-- 2# button -> Vai a visite.php -->
-        <li class="nav-item">
-          <a class="nav-link" href="visite.php">Visite</a>
-        </li>
-        
-        <!-- 3# button -> Vai a profilo.php/logout.php -->
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Profilo
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="profilo.php">Visualizza</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="<?php echo"$LOGOUT_PATH"?>">Logout</a></li>
-          </ul>
-        </li>
-        
-      </ul>
-      <!-- 
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>-->
+            <!-- Torna a home.php -->
+            <div class="col-1">
+              <a class="navbar-brand" href="home.php">
+                <img src="<?php echo"$LOGO_PATH"?>" alt="Logo" width="50" height="50">
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+
+            <div class="col-2">
+              <!-- 2# button -> Vai a visite.php -->
+              <li class="nav-item">
+                <a class="nav-link" href="visite.php">Visite</a>
+              </li>
+            </div>
+
+            <div class="col-2">
+              <!-- 3# button -> Vai a profilo.php/logout.php -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Profilo
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="profilo.php">Visualizza</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="<?php echo"$LOGOUT_PATH"?>">Logout</a></li>
+                </ul>
+              </li>
+            </div>
+
+        </ul>
+        <!-- 
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>-->
+
+      </div>
     </div>
   </nav>
     
