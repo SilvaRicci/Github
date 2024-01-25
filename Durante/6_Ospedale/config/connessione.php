6b860f6c6350d7c713c069809ab769a74fd1b647
root <samueleorazio.durante@gmail.com>
2024-01-25 17:33:27 +0000

    auto-push

--- Durante/6_Ospedale/config/connessione.php
+++ Durante/6_Ospedale/config/connessione.php
@@ -10,5 +10,3 @@ $db_connection = new mysqli($db_host,$db_user,$db_password,$db);
 if($db_connection->connect_error){                                                                          
     die("Si e' verificato il seguente problema tecnico: " . $db_connection->connect_error);                 
 }                                                                                                           
-
-?>
