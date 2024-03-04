e020e0eea1498966f7e18447da9688a8012921aa
root <samueleorazio.durante@gmail.com>
2024-03-04 08:14:24 +0000

    auto-push

--- Durante/8_OnlineShop/index.php
+++ Durante/8_OnlineShop/index.php
@@ -62,44 +62,41 @@
         ?>
 
     <div class="container">
-        <?php
-            foreach($result as $item){
+        <div class="row">
+            <?php foreach($result as $item):?>
+                <div class="col-3">
+                        <form action="#" method="POST">
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?php echo $item['img']; ?>" class="card-img-top" alt="<?php echo $item['nome_prodotto']; ?> bello/a">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $item['nome_prodotto']; ?></h5>
 
-            }
-        ?>
+                                    <br>
 
-            <div class="col">
-                    <form action="#" method="POST">
-                        <div class="card" style="width: 18rem;">
-                            <img src="$item[img]" class="card-img-top" alt="'.$row["nome_prodotto"].' bello/a">
-                            <div class="card-body">
-                                <h5 class="card-title">'.$row["nome_prodotto"].'</h5>
+                                    <div class="input-group">
+                                        <span class="input-group-text">Quantità</span>
+                                        <input type="number" class="form-control" id="quantita" name="quantita">
+                                    </div>
 
-                                <br>
+                                    <br>
 
-                                <div class="input-group">
-                                    <span class="input-group-text">Quantità</span>
-                                    <input type="number" class="form-control" id="quantita" name="quantita">
+                                    <div class="input-group">
+                                        <span class="input-group-text">€</span>
+                                        <span class="input-group-text"><?php echo $item['pvu_prodotto']; ?></span>
+                                    </div>
+
+                                    <input type="hidden" name="id" id="id" value='<?php echo $item['id_prodotto']; ?>' readonly>
+
+                                    <br>
+
+                                    <button type="submit" id="submit_btn" name="submit_btn" class="btn btn-primary">Aggiungi al carrello</button>
                                 </div>
-
-                                <br>
-
-                                <div class="input-group">
-                                    <span class="input-group-text">€</span>
-                                    <span class="input-group-text">'.$row["pvu_prodotto"].'</span>
-                                </div>
-
-                                <input type="hidden" name="id" id="id" value='.$row["id_prodotto"].' readonly>
-
-                                <br>
-
-                                <button type="submit" id="submit_btn" name="submit_btn" class="btn btn-primary">Aggiungi al carrello</button>
                             </div>
-                        </div>
-                    </form>
-                </div>
-
-
+                        </form>
+                    </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 <?php 
         /*
         $nCol = 0;
