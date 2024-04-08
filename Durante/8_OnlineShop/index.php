28971b140818f32d3e44b44c4e849935f01de40f
root <samueleorazio.durante@gmail.com>
2024-04-08 17:34:16 +0000

    auto-push

--- Durante/8_OnlineShop/index.php
+++ Durante/8_OnlineShop/index.php
@@ -13,15 +13,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        body {
-            background-image: url('src/img/background.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-attachment: fixed;
-        }
-    </style>
     <title>Homepage</title>
+    <link rel="stylesheet" href="src/css/base.css">
     <link rel="stylesheet" href="src/css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
