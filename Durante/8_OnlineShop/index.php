4f986a1381be181b38cd7244cd7f2128d4eef564
root <samueleorazio.durante@gmail.com>
2024-04-08 07:05:32 +0000

    auto-push

--- Durante/8_OnlineShop/index.php
+++ Durante/8_OnlineShop/index.php
@@ -76,39 +76,6 @@
                         <img src="src/img/account.png" alt="Account" height="45px" width="45px">
                     </a>
                 </li>
-
-                        <!-- Navbar dropdown -->
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <!-- Dropdown menu -->
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a class="dropdown-item" href="#">Action</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">Another action</a>
-            </li>
-            <li><hr class="dropdown-divider" /></li>
-            <li>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
-            >Disabled</a
-          >
-        </li>
-
             </ul>
             <!-- Left links -->
             </div>
