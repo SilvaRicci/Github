cf45e40d49906362bdf26f85db3e00ea3f5be35b
root <samueleorazio.durante@gmail.com>
2024-04-18 16:47:09 +0000

    auto-push

--- Durante/8_OnlineShop/newsletter.php
+++ Durante/8_OnlineShop/newsletter.php
@@ -1,9 +1,13 @@
-<?php
-    include "src.php";
-    
-    $email = $_POST["email-newsletter"];
-    addToNewsletter($email);
+<html>
+    <body>
+        <?php
+            include "src.php";
+            
+            $email = $_POST["email-newsletter"];
+            addToNewsletter($email);
 
-    alert("Iscrizione avvenuta con successo! Buona giornata.");
-
-    header("Location: index.php");
+            alert("Iscrizione avvenuta con successo! Buona giornata.");
+            echo "window.setTimeout(function(){window.location.href = 'index.php';}, 5000);"
+        ?>
+    </body>
+</html>
