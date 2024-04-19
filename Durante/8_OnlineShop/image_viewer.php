45a1f1a43181bc347bffdb9dbc161d161f8033bc
root <samueleorazio.durante@gmail.com>
2024-04-19 09:05:59 +0000

    auto-push

--- Durante/8_OnlineShop/image_viewer.php
+++ Durante/8_OnlineShop/image_viewer.php
@@ -1,14 +1,12 @@
 <?php
-require_once __DIR__ . '/db.php';
-if (isset($_GET['image_id'])) {
-    $sql = "SELECT imageType,imageData FROM tbl_image WHERE imageId=?";
-    $statement = $conn->prepare($sql);
-    $statement->bind_param("i", $_GET['image_id']);
-    $statement->execute() or die("<b>Error:</b> Problem on Retrieving Image BLOB<br/>" . mysqli_connect_error());
-    $result = $statement->get_result();
+    include "connessione.php";
 
-    $row = $result->fetch_assoc();
-    header("Content-type: " . $row["imageType"]);
-    echo $row["imageData"];
-}
+    $query = "SELECT * FROM `tbl_image` WHERE 1";
+    $result = $db_connection->query($query);
+    $row = $result->fetch_array();
+
+    echo 
+    `<img src = "data:image/png;base64,' . base64_encode(`.$row['imageData'].`) . '" width = "80px" height = "80px"/>`
 ?>
+
+
