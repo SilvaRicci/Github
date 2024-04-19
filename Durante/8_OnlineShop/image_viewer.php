92262a60be0130fe98c03b57a4a9e0cb30bc1a86
root <samueleorazio.durante@gmail.com>
2024-04-19 09:26:05 +0000

    auto-push

--- Durante/8_OnlineShop/image_viewer.php
+++ Durante/8_OnlineShop/image_viewer.php
@@ -1,12 +1,9 @@
 <?php
     include "connessione.php";
 
-    $query = "SELECT * FROM `tbl_image` WHERE 1";
+    $query = "SELECT imageData FROM `tbl_image` WHERE `imageId` = '5'";
     $result = $db_connection->query($query);
     $row = $result->fetch_array();
-
-    echo 
-    `<img src = "data:image/png;base64,' . base64_encode(`.$row['imageData'].`) . '" width = "80px" height = "80px"/>`
 ?>
-
+<img src="sesso" width = "80px" height = "80px/> 
 
